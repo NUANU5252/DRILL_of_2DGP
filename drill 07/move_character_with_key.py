d2256ab6b6b058ca4d539887df5bc9e87402e6ec
NUANU5252 <84632916+NUANU5252@users.noreply.github.com>
2021-09-30 18:50:40 +0900

    move_with_key 수정
    움직이는 방향과 속도에 따른 4가지 애니메이션

--- drill 07/move_character_with_key.py
+++ drill 07/move_character_with_key.py
@@ -4,22 +4,25 @@ from pico2d import *
 def handle_events():
     global running
     global dir
+    global speed
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
-                dir += 1
+                dir = 2
+                speed += 1
             elif event.key == SDLK_LEFT:
-                dir -= 1
+                dir = 1
+                speed -= 1
             elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
-                dir -= 1
+                speed -= 1
             elif event.key == SDLK_LEFT:
-                dir += 1
+                speed += 1
 
 open_canvas()
 grass = load_image('grass.png')
@@ -28,18 +31,25 @@ character = load_image('animation_sheet.png')
 running = True
 x = 800 // 2
 frame = 0
-dir = 0 # 방향 -1 = left +1 right
-
+dir = 2 # 방향 1 = left 2 right
+speed = 0 # x축의 속도 양수 right 음수 left
+status = 0 # 0 : 우측 기본, 1 : 좌측 기본, 2 : 우측 이동, 3 : 좌측 이동
 
 while running:
     clear_canvas()
     grass.draw(400, 30)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, 90)
+    if speed > 0:
+        status = 1
+    elif speed < 0:
+        status = 0
+    else:
+        status = dir + 1
+    character.clip_draw(frame * 100, 100 * status, 100, 100, x, 90)
     update_canvas()
 
     handle_events()
     frame = (frame + 1) % 8
-    x += dir * 5
+    x += speed * 5
     delay(0.01)
 
 close_canvas()
